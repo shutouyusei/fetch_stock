1f4826735950d3a2c3db21cb3ca4518f5d2de63b
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-04-20 13:53:41 +0900

    [add]コマンドラインでの引数処理

--- Fetch_stock_py/Fetch_stock_py.py
+++ Fetch_stock_py/Fetch_stock_py.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from asyncio.windows_events import NULL
 import os
 import datetime as dt
 import pandas_datareader.data as web
- 
-ticker_symbol="7177"
-ticker_symbol_dr=ticker_symbol + ".JP"
- 
-start='2022-01-01'
+import sys
+
+
 end = dt.date.today()
- 
+start=dt.datetime(end.year-2,end.month,end.day)
+print(start)
+print(end)
+if __name__ == '__main__':
+    args = sys.argv 
+    ticker_symbol=args[1]
+    if ticker_symbol!=NULL:
 
-df = web.DataReader(ticker_symbol_dr, data_source='stooq', start=start,end=end)
+            ticker_symbol_dr=ticker_symbol + ".JP"
 
-df.insert(0, "code", ticker_symbol, allow_duplicates=False)
+            df = web.DataReader(ticker_symbol_dr, data_source='stooq', start=start,end=end)
 
-df.to_csv( os.path.dirname(__file__) + '\s_stock_data_'+ ticker_symbol + '.csv', encoding="shift-jis")
+            df.insert(0, "code", ticker_symbol, allow_duplicates=False)
+
+            df.to_csv( os.path.dirname(__file__) + '\s_stock_data_'+ ticker_symbol + '.csv', encoding="shift-jis")
 
 
-print(df)
-print("success")
+            print(df)
+            print("success")
+    else:
+        print ("ticker is null")
+        
