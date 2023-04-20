317f860901f901d70cd1c1107ac080420ad38de2
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-04-20 14:02:00 +0900

    [add]start,endの変数への処理、dateframeがempty出会った時の処理

--- Fetch_stock_py/Fetch_stock_py.py
+++ Fetch_stock_py/Fetch_stock_py.py
@@ -3,19 +3,21 @@
 from asyncio.windows_events import NULL
 import os
 import datetime as dt
+from tkinter import N
 import pandas_datareader.data as web
 import sys
 
 
-end = dt.date.today()
-start=dt.datetime(end.year-2,end.month,end.day)
-print(start)
-print(end)
+
 if __name__ == '__main__':
     args = sys.argv 
     ticker_symbol=args[1]
     if ticker_symbol!=NULL:
 
+           # get today date
+            end = dt.date.today()
+            start=dt.datetime(end.year-2,end.month,end.day)
+            # accesse pandas_datareader to get stock imf
             ticker_symbol_dr=ticker_symbol + ".JP"
 
             df = web.DataReader(ticker_symbol_dr, data_source='stooq', start=start,end=end)
@@ -25,8 +27,13 @@ if __name__ == '__main__':
             df.to_csv( os.path.dirname(__file__) + '\s_stock_data_'+ ticker_symbol + '.csv', encoding="shift-jis")
 
 
-            print(df)
-            print("success")
+            # output 
+            if df.empty==True:
+                print("NUll")
+            else:
+
+                print(df)
+                print("success")
     else:
         print ("ticker is null")
         
