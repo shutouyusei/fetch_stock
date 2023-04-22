41f035cec5c7d49994f8d11752e36fd432fb9669
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-04-22 10:42:19 +0900

    [fix]csvファイルの出力先の調整

--- Fetch_stock_py/Fetch_stock_py.py
+++ Fetch_stock_py/Fetch_stock_py.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from asyncio.windows_events import NULL
+from genericpath import exists
 import os
 import datetime as dt
 from tkinter import N
@@ -13,7 +14,6 @@ if __name__ == '__main__':
     args = sys.argv 
     ticker_symbol=args[1]
     if ticker_symbol!=NULL:
-
            # get today date
             end = dt.date.today()
             start=dt.datetime(end.year-2,end.month,end.day)
@@ -23,17 +23,18 @@ if __name__ == '__main__':
             df = web.DataReader(ticker_symbol_dr, data_source='stooq', start=start,end=end)
 
             df.insert(0, "code", ticker_symbol, allow_duplicates=False)
-
-            df.to_csv( os.getcwd() + '\s_stock_data_'+ ticker_symbol + '.csv', encoding="shift-jis")
-
-            print(os.getcwd())
+            
             # output 
             if df.empty==True:
                 print("NUll")
             else:
+                # mkdir on cd 
+                os.makedirs( os.getcwd()+'\csv_stock_data',exist_ok=True)
 
-                print(df)
+                # create csv stock_data
+                df.to_csv( os.getcwd() + '\csv_stock_data\s_stock_data_'+ ticker_symbol + '.csv', encoding="shift-jis")
                 print("success")
     else:
+        # no ticker
         print ("ticker is null")
         
